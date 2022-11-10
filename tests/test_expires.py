293713d712de0539928f18a9044e844efad37627
Matthew Albrecht <matthewwilliamalbrecht@gmail.com>
2022-11-10 11:36:23 -0500

    remove loop & fix version

--- tests/test_expires.py
+++ tests/test_expires.py
@@ -7,8 +7,8 @@ from async_lru import alru_cache
 pytestmark = pytest.mark.asyncio
 
 
-async def test_expires(check_lru, loop):
-    @alru_cache(maxsize=4, expires=0.2, loop=loop)
+async def test_expires(check_lru):
+    @alru_cache(maxsize=4, expires=0.2)
     async def coro(val):
         return val
 
@@ -17,19 +17,19 @@ async def test_expires(check_lru, loop):
     assert await coro(value) == value
     check_lru(coro, hits=0, misses=1, cache=1, tasks=0, maxsize=4)
 
-    await asyncio.sleep(0.1, loop=loop)
+    await asyncio.sleep(0.1)
     assert await coro(value) == value
     check_lru(coro, hits=1, misses=1, cache=1, tasks=0, maxsize=4)
 
-    await asyncio.sleep(0.3, loop=loop)
+    await asyncio.sleep(0.3)
     # cache is clear after time expires
     check_lru(coro, hits=1, misses=1, cache=0, tasks=0, maxsize=4)
     assert await coro(value) == value
     check_lru(coro, hits=1, misses=2, cache=1, tasks=0, maxsize=4)
 
 
-async def test_expires_maxsize(check_lru, loop):
-    @alru_cache(maxsize=1, expires=0.2, loop=loop)
+async def test_expires_maxsize(check_lru):
+    @alru_cache(maxsize=1, expires=0.2)
     async def coro(val):
         return val
 
@@ -39,7 +39,7 @@ async def test_expires_maxsize(check_lru, loop):
     assert await coro(2) == 2
     check_lru(coro, hits=0, misses=2, cache=1, tasks=0, maxsize=1)
 
-    await asyncio.sleep(0.1, loop=loop)
+    await asyncio.sleep(0.1)
     assert await coro(2) == 2
     check_lru(coro, hits=1, misses=2, cache=1, tasks=0, maxsize=1)
 
